422480faa6bcd2d132af0ce8f3cd80afd6424ba0
Timmy <mr.oshnokov@gmail.com>
2020-04-15 16:18:08 +0300

    Exceptions

--- src/Lab2_Lab3_Lab4/ru/billing/stocklist/ItemCatalog.java
+++ src/Lab2_Lab3_Lab4/ru/billing/stocklist/ItemCatalog.java
@@ -9,15 +9,18 @@ public class ItemCatalog {
             new HashMap<Integer, GenericItem>();
     private ArrayList<GenericItem> ALCatalog =
             new ArrayList<GenericItem>();
+    private HashMap<Pair<String, Category>, GenericItem> elseCatalog = new HashMap<>();
 
     public void addItem(GenericItem item) throws ItemAlreadyExistsException {
 
-        if (catalog.containsKey(item.getID()) || ALCatalog.contains(item)) throw new ItemAlreadyExistsException();
+        if (catalog.containsKey(item.getID()) || ALCatalog.contains(item) || elseCatalog.containsKey(new Pair<>(item.getName(), item.getCategory())))
+            throw new ItemAlreadyExistsException();
 
         else {
 
             catalog.put(item.getID(), item);
             ALCatalog.add(item);
+            elseCatalog.put(new Pair<>(item.getName(), item.getCategory()), item);
         }
     }
 
@@ -49,20 +52,37 @@ public class ItemCatalog {
         }
     }
 
-    public GenericItem specialSearchAL(String name, Category category) {
+    public GenericItem[] specialSearchAL(String name, Category category) {
+        GenericItem[] resultArray = new GenericItem[ALCatalog.size()];
+        int count = 0;
         for (GenericItem i : ALCatalog)
             if (i.getName().equals(name) && i.getCategory() == category)
-                return i;
-        return null;
+                resultArray[count++] = i;
+        return resultArray;
     }
 
     public GenericItem specialSearch(String name, Category category) {
+//        GenericItem[] resultArray = new GenericItem[catalog.size()];
+//        int count = 0;
+        return elseCatalog.getOrDefault(new Pair<>(name, category), null);
 
-        for (Map.Entry<Integer, GenericItem> pair : catalog.entrySet()) {
-            if (pair.getValue().getName().equals(name) && pair.getValue().getCategory() == category)
-                return pair.getValue();
-        }
-        return null;
+    }
+
+}
+
+class Pair<T, S> {
+    public T firstValue;
+    public S secondValue;
+
+    Pair(T firstValue, S secondValue) {
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Pair newobj = (Pair) obj;
+        return (this.firstValue.equals(newobj.firstValue)) && (this.secondValue.equals(newobj.secondValue));
     }
 }
 
